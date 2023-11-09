9abdf6dec22ec611c57e31b8bf0e7c238b264159
Jean-Sylvain Boige <jsboige@gmail.com>
2023-11-09 12:33:33 +0100

    Corrections pour Mouhammed (AndreiKolmogorov)

--- Algorithm.CSharp/AndreiKolmogorov.cs
+++ Algorithm.CSharp/AndreiKolmogorov.cs
@@ -1,25 +1,55 @@
 using QuantConnect.Algorithm;
+using QuantConnect.Brokerages;
 using QuantConnect.Data;
 using QuantConnect.Indicators;
 using QuantConnect.Data.Market;
+using System;
+using System.Drawing;
 
 namespace QuantConnect
 {
     public class AndreiKolmogorov : QCAlgorithm
     {
         private MovingAverageConvergenceDivergence _macd;
-        private readonly string _btcusd = "BTCUSD";
+        private Symbol _btcusd;
         private const decimal _tolerance = 0.0025m;
         private bool _invested;
 
+        private string _ChartName = "Trade Plot";
+        private string _PriceSeriesName = "Price";
+        private string _PortfoliovalueSeriesName = "PortFolioValue";
+
         public override void Initialize()
         {
-            SetStartDate(2023, 1, 1); // début backtest
-            SetEndDate(2023, 12, 31); // fin backtest
+            SetStartDate(2020, 1, 1); // début backtest
+            SetEndDate(2023, 10, 31); // fin backtest
+
+
+            SetBrokerageModel(BrokerageName.Bitstamp, AccountType.Cash);
+
             SetCash(10000); // capital
 
-            AddCrypto(_btcusd, Resolution.Daily);
+            _btcusd = AddCrypto("BTCUSD", Resolution.Daily).Symbol;
+
+
             _macd = MACD(_btcusd, 12, 26, 9, MovingAverageType.Exponential, Resolution.Daily, Field.Close);
+
+            
+            // Dealing with plots
+            var stockPlot = new Chart(_ChartName);
+            var assetPrice = new Series(_PriceSeriesName, SeriesType.Line, "$", Color.Blue);
+            var portFolioValue = new Series(_PortfoliovalueSeriesName, SeriesType.Line, "$", Color.Green);
+            stockPlot.AddSeries(assetPrice);
+            stockPlot.AddSeries(portFolioValue);
+            AddChart(stockPlot);
+            Schedule.On(DateRules.EveryDay(), TimeRules.Every(TimeSpan.FromDays(1)), DoPlots);
+
+        }
+
+        private void DoPlots()
+        {
+            Plot(_ChartName, _PriceSeriesName, Securities[_btcusd].Price);
+            Plot(_ChartName, _PortfoliovalueSeriesName, Portfolio.TotalPortfolioValue);
         }
 
         public override void OnData(Slice data)
@@ -31,12 +61,15 @@ namespace QuantConnect
             if (holdings <= 0 && _macd > _macd.Signal * (1 + _tolerance)) // condition d'achat
             {
                 SetHoldings(_btcusd, 1.0);
+
+                Debug($"Purchased BTC @{data.Bars[_btcusd].Close}$/Btc; Portfolio: {Portfolio.Cash}$, {Portfolio[_btcusd].Quantity}BTCs, Total Value: {Portfolio.TotalPortfolioValue}$, Total Fees: {Portfolio.TotalFees}$");
                 _invested = true;
             }
             else if (_invested && _macd < _macd.Signal) // condition de vente
             {
                 Liquidate(_btcusd);
                 _invested = false;
+                Debug($"Sold BTC @{data.Bars[_btcusd].Close}$/Btc; Portfolio: {Portfolio.Cash}$, {Portfolio[_btcusd].Quantity}BTCs, Total Value: {Portfolio.TotalPortfolioValue}$, Total Fees: {Portfolio.TotalFees}$");
             }
         }
     }
